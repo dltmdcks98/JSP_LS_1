5677ddcb87a55d4bcb2bc5449a0057e5bd25b8b7
dltmdcks98 <dltmdcks98@gmail.com>
2022-09-01 10:45:06 +0900

    22.09.01 if문 제거

--- model2app/src/main/java/com/academy/model2app/controller/DispatcherServlet.java
+++ model2app/src/main/java/com/academy/model2app/controller/DispatcherServlet.java
@@ -1,6 +1,9 @@
 package com.academy.model2app.controller;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Properties;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,8 +20,27 @@ import com.academy.model2app.movie.model.MovieManager;
  * 모든 요청마다 1:1 대응하는 컨트롤러를 전면에 내세우면 오히려 유지 보수성이 떨어진다.
  */
 public class DispatcherServlet extends HttpServlet{
+	FileInputStream fis;
+	Properties props;
 	
-
+	//생성자, init()으로 fis에 메모리에 올린다. doXX에 올리지 않는 이유는 요청이 올때마다 메모리에  File을 올리는건 낭비여서 서블릿이실행될때 처음만 실행되도록 하기 위해
+	@Override
+	public void init() throws ServletException {
+		try {
+			String path = "D:/OneDrive-LSC/SLAcademy/JSP_LS_1/model2app/src/main/webapp/WEB-INF/mapping.properties";
+			fis= new FileInputStream(path);
+			props = new Properties();
+			props.load(fis);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	//모든 요청을 이 서블릿이 받아야한다.
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doRequest(req, resp);
@@ -34,6 +56,26 @@ public class DispatcherServlet extends HttpServlet{
 		System.out.println(uri);
 		Controller controller = null;
 		//2)요청 분석
+		//주의) 검색 결과는 컨트롤러 클래스 자체가 아닌, 단지 그 클래스의 경로일 뿐이다.(String형)
+		String controllerClassName = props.getProperty(uri);
+		//String 경로를 이용하여 실제 클래스의 인스턴스를 생성하는 법 
+		try {
+			//()안에 있는 경로를 통해 static 영역으로 올린다. 원본만 올리는 것 아직 인스턴스가 올라오지 않음, new를 하지 않음 
+			Class controllerClass=Class.forName(controllerClassName);
+			controller =(Controller) controllerClass.newInstance();//new 연산자를 만드는 법, 구버젼
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		/*
 		if(uri.equals("/movie.do")) {	//영화에 대한 요청인지
 			//영화 전담자인 MovieController 에게 업무를 맡긴다.
 			controller = new BloodController();
@@ -41,13 +83,25 @@ public class DispatcherServlet extends HttpServlet{
 		}else if(uri.equals("/blood.do")) {	//혈액형에 대한 요청인지
 			
 		}
+		*/
+		
 		controller.execute(request, response);
 		//5)알맞은 뷰 페이지
 		RequestDispatcher dis = request.getRequestDispatcher(controller.getViewPage());
 		dis.forward(request, response);
-		
 
 	}
 	
+	//서블릿이 일을 다하고 소면될때, 호출되는 생명주기 메서드
+	public void destroy() {
+		if(fis!=null) {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 	
 }
