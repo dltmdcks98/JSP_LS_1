9e4455065b8517d8c022d4893e61f3ae521b70b2
dltmdcks98 <dltmdcks98@gmail.com>
2022-09-01 15:17:18 +0900

    22.09.01 DispatcherServlet.java를 서버가 켜질때 실행으로 변경

---
+++ model2app/src/main/java/com/academy/model2app/listener/MyListener.java
@@ -0,0 +1,43 @@
+package com.academy.model2app.listener;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/*
+ * 서버가 가동될때 하고 싶을때
+ */
+public class MyListener implements ServletContextListener{
+	FileInputStream fis;
+	Properties props;
+	
+	//웹 컨테이너가 가동 될때 호출되는 메서드
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext context = sce.getServletContext();//jsp 에서의 application 내장객체의 자료형
+		String path = context.getInitParameter("contextConfigLocation");
+
+		try {
+			fis = new FileInputStream(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//웹 컨테이너가 중단될때 호출되는 메서드
+	public void contextDestroyed(ServletContextEvent sce) {
+		if(fis!=null) {
+			try {
+				fis.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ model2app/src/main/java/com/academy/model2app/model/repository/NoticeDAO.java
@@ -0,0 +1,13 @@
+package com.academy.model2app.model.repository;
+
+import com.academy.model2app.domain.Notice;
+
+/*
+ * MVC 중 모델 영역인 DAO를 정의
+ */
+public class NoticeDAO {
+	
+	public void insert(Notice notice) {
+
+	}
+}
